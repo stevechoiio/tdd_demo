5185e4e13540045a0ea6d32c4a770709c8596405
Steve Choi <stevechoi93@gmail.com>
2019-02-20 15:37:47 -0800

    fixed spacing error

--- _tests_/palindrome.test.js
+++ _tests_/palindrome.test.js
@@ -1,7 +1,7 @@
 const palindrome = string => {
-  let reversed = [...string].reverse().join("");
-  console.log(reversed);
-  return string === reversed;
+  let filteredString = [...string].filter(a => /[a-z]/.test(a));
+  let reversed = [...filteredString].reverse().join("");
+  return filteredString.join("") === reversed;
 };
 
 describe("palindrome", () => {
@@ -11,6 +11,10 @@ describe("palindrome", () => {
   });
   test("when it has even letters", () => {
     const result = palindrome("abba");
+    expect(result).toEqual(true);
+  });
+  test("when it has lots of spaces", () => {
+    const result = palindrome("ne v    er eve n");
     expect(result).toEqual(true);
   });
 });
